6f96e05fadcbbdc862957aafe900bce357a980d4
August Forsman <august.forsman@savantic.se>
2024-09-14 23:05:46 +0200

    LaTeX SUPPORT

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -1,49 +1,42 @@
 return {
-  {
-    "lervag/vimtex",
-    ft = { "tex" },  -- Load vimtex only for TeX files
-    config = function()
-      -- VimTeX Compiler Configuration
-      vim.g.vimtex_compiler_latexmk = {
-        build_dir = '',
-        callback = 1,
-        continuous = 1,
-        executable = 'latexmk',
-        hooks = {},
-        options = {
-          '-pdf',
-          '-lualatex',
-          '-shell-escape',
-        },
-      }
-
-      -- Enable automatic compilation on save
-      vim.g.vimtex_compiler_auto = 1
-
-      -- Set the main file (replace 'main.tex' with your actual main LaTeX file)
-      vim.g.tex_flavor = 'latex'
-      vim.g.vimtex_mainfile = 'main.tex'
-
-      -- Enable folding of sections and environments
-      vim.g.vimtex_fold_enabled = 1
-
-      -- Use Neovim terminal for compilation
-      vim.g.vimtex_quickfix_open_on_warning = 0
-      vim.g.vimtex_quickfix_open_on_error = 1
-
-      -- Use Zathura as the PDF viewer (replace with your preferred viewer)
-      vim.g.vimtex_view_method = 'zathura'
-
-      -- Enable completion for bibtex references
-      vim.g.vimtex_complete_bib = 1
-
-      -- Automatically close preview windows
-      vim.g.vimtex_quickfix_autoclose = 1
-
-      -- Set up mappings for compiling and viewing
-      vim.api.nvim_set_keymap('n', '<Leader>ll', '<Plug>Tex_LaTeX', {})
-      vim.api.nvim_set_keymap('n', '<Leader>lv', '<Plug>Tex_View', {})
-      vim.api.nvim_set_keymap('n', '<Leader>lc', '<Plug>Tex_Clean', {})
-    end,
-  }
+  'lervag/vimtex',
+  ft = { "tex" },
+  config = function()
+    vim.g.vimtex_view_method = 'zathura'  -- or your preferred PDF viewer
+    vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_auto = 1
+    vim.g.vimtex_mainfile = 'main.tex'
+    vim.g.vimtex_fold_enabled = 1
+    vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_quickfix_open_on_error = 1
+    -- vim.g.vimtex_complete_bib = 1
+    vim.g.vimtex_quickfix_autoclose = 1
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = '',
+      callback = 1,
+      continuous = 1,
+      executable = 'latexmk',
+      hooks = {},
+      options = {
+        '-pdf',
+        '-lualatex',
+        '-shell-escape',
+      },
+    }
+  end
 }
+-- return {
+--   {
+--     config = function()
+--
+--       -- Enable automatic compilation on save
+--
+--       -- Set the main file (replace 'main.tex' with your actual main LaTeX file)
+--
+--       -- Set up mappings for compiling and viewing
+--       vim.api.nvim_set_keymap('n', '<Leader>ll', '<Plug>Tex_LaTeX', {})
+--       vim.api.nvim_set_keymap('n', '<Leader>lv', '<Plug>Tex_View', {})
+--       vim.api.nvim_set_keymap('n', '<Leader>lc', '<Plug>Tex_Clean', {})
+--     end,
+--   }
+-- }
